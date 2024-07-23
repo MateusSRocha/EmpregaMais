96d59d9541e77f46601f3629935ff1b3ea3d3942
Nafanael Pereira Alves <126331218+nafanael@users.noreply.github.com>
2024-07-23 08:59:01 -0300

    adalto

--- public/views/src/index.php
+++ public/views/src/index.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Você está logado!</title>
+    <title>Inicio</title>
     <link rel="stylesheet" href="public/assets/css/src/style.css">
 </head>
 <body>
+    <div class="fundo">
+        <header>
+            <div class="logo">
+                <h1>EMPREGA<span>MAIS</span></h1>
+            </div>
+            <div class="cabecalho_link">
+                <li>
+                    <a href="cadastro.php">Cadastrar curriculo</a>
+                </li>
+            </div>
+        </header>
     
 </body>
 </html>
