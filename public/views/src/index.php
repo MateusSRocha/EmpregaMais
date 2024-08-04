9f21da6212cb26a547fc9a7793f0214feef199c8
Mateus Soares da Rocha Cordeiro <msgdhdjf777@gmail.com>
2024-08-04 10:51:41 -0300

    textes

--- public/views/src/index.php
+++ public/views/src/index.php
@@ -10,7 +10,7 @@
     <div class="fundo">
         <header>
             <div class="logo">
-                <h1>EMPREGA<span>MAIS</span></h1>
+                <h1>EMPREGA<span>MAIS - CLIENTE</span></h1>
             </div>
             <div class="cabecalho_link">
                 <li>
@@ -23,6 +23,20 @@
                 </li>
             </div>
         </header>
-    
+        <main>
+            <div class="empresas-recrutando">
+                <h2>Empresas que estão recrutando</h2>
+                <div class="empresa">
+                    <h3>Empresa A</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore repellat quas magnam doloremque ducimus est unde odio natus facere quam ratione ullam, inventore rem illum, deserunt deleniti voluptatum suscipit aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nobis voluptatibus ex repellat nam et, blanditiis dolore voluptatum, reiciendis quia esse tempore ipsum eum harum ad aperiam eius impedit asperiores.</p>
+                    <button type="button" onclick="location.href='enviar_curriculo.php?empresa=EmpresaA'">Enviar currículo</button>
+                </div>
+                <div class="empresa">
+                    <h3>Empresa B</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore repellat quas magnam doloremque ducimus est unde odio natus facere quam ratione ullam, inventore rem illum, deserunt deleniti voluptatum suscipit aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nobis voluptatibus ex repellat nam et, blanditiis dolore voluptatum, reiciendis quia esse tempore ipsum eum harum ad aperiam eius impedit asperiores.</p>
+                    <button type="button" onclick="location.href='enviar_curriculo.php?empresa=EmpresaB'">Enviar currículo</button>
+                </div>
+            </div>
+        </main>
 </body>
 </html>
